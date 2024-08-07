beeb5b40446d16b45bcdcb35db1610075e4969c5
DiegoDzm <diegodiazmed93@gmail.com>
2024-08-07 15:30:35 -0400

    1

---
+++ first-app/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import './App.css';
+import Home from "./views/home";
+import injectContext from './store/context';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path='/'  element={<Home/>}></Route>
+    </Routes>
+   </BrowserRouter>
+
+
+  );
+}
+
+export default injectContext(App);
---
+++ first-app/src/views/home.js
@@ -0,0 +1,17 @@
+import { Context } from "../store/context"
+import { useContext,useEffect } from "react"
+
+
+
+
+
+const Home =()=>{
+
+    return <div className="container">{
+       <h2>hello world</h2>
+}</div>
+
+
+};
+
+export default Home
