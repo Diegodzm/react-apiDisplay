37feeb6e7bd5790f121f5f3d7abfeae0df9f565c
DiegoDzm <diegodiazmed93@gmail.com>
2024-08-12 15:14:03 -0400

    back

--- first-app/back/app.py
+++ first-app/back/app.py
@@ -12,7 +12,7 @@ app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///" +os.path.join(BASEDIR,"blog.db")
 app.config['JWT_SECRET_KEY']= 'secret-code-01'
 db.init_app(app)
-Migrate(app,db)
+migrate=Migrate(app,db)
 CORS(app)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
@@ -22,33 +22,8 @@ jwt = JWTManager(app)
 def home():
     return 'hello'
 
-@app.route('/user/register', methods=['POST'])
-def user_register():
-     email= request.json.get('email')
-     if email is not None:
-        user = User.query.filter_by(email=email).first()
-        if user is not None:
-            return jsonify({
-                "msg":"Este usuario ya existe"
-                }),400
-        
-        else:
-            user=User()
-            password= request.json.get('password')
-            password_hash= bcrypt.generate_password_hash(password)
 
-            user.password =password_hash
-            user.email= email
+if __name__  == '__main__':
 
-
-            db.session.add(user)
-            db.session.commit()
-
-            return jsonify({
-                "msg":"user created"
-            }), 200
-
-     else:
-        return jsonify({
-            "msg": "email is required"
-        }),400
+    app.run(host='localhost',port=5000)
+    app.run(host='localhost',port=5000,debug=True)
